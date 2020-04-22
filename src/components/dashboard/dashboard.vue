d7ce4524b89d33d7aa92aa88b1e09586eed0264f
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-22 20:23:38 +0800

    透過 axios.get 取得 API 資料實施例

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -2,8 +2,47 @@
   <div id="dashboard">
     <h1>That's the dashboard!</h1>
     <p>You should only get here if you're authenticated!</p>
+    <p>Your mail address: {{ email }}</p>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() { // ES6可簡寫成: data() {}
+    return {
+      email: ''
+    }
+  }, 
+  computed: function() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  created: function() { // vue lifecycle hook: created
+    axios.get('https://vuejs-course-lab22-axios.firebaseio.com/users.json')
+      .then(response => {
+        console.log(response);
+        const data = response.data;
+        const users = [];
+        for(let key in data) {
+          const oneUser = data[key];
+          oneUser.id = key;
+          users.push(oneUser);
+        }
+        console.log(users);
+        this.email = users[0].email;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+}
+</script>
 
 <style scoped>
   h1, p {
