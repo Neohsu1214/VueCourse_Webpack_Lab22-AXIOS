1b923b9d9225e07683faa8a6badbb1693d652f27
Neo Hsu <neohsu@Neode-Mac-mini.local>
2020-04-24 00:16:07 +0800

    axios.interceptor可透過eject來取消，但是要事先建立變數去接.use 回傳的ID

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ axios.defaults.headers.common['Authorization'] = 'neoishandsome' // headers.comm
 axios.defaults.headers.get['Accept'] = 'application/json' // headers.get: 設定 get request 的 header 都要有的參數
 
 // 建立 axios.interceptors 來對每次的 requeest 做些處理
-axios.interceptors.request.use(config => {
+const requeestInterceptor = axios.interceptors.request.use(config => {
   // Do something before request is sent
   console.log('Axios Interceptors Request', config);
   config.headers['INTERCEPTOR_ADDED_HEADER'] = 'yes I can!'; // 以此例就是在每次 axios request 時安插一個 header
@@ -25,7 +25,7 @@ axios.interceptors.request.use(config => {
 });
 
 // 建立 axios.interceptors 來對每次的 response 做些處理
-axios.interceptors.response.use(response => {
+const responseInterceptor = axios.interceptors.response.use(response => {
   // Do something before response is sent
   console.log('Axios Interceptors Response', response);
   return response;
@@ -33,6 +33,10 @@ axios.interceptors.response.use(response => {
   // Do something with response error
   return Promise.reject(error);
 });
+
+// axios 可透過 interceptors.xxx.eject 來跳脫所設定的 interceptor，但是原本宣告 interceptor 的地方要建立變數去接 .use 回傳的 interceptor ID
+axios.interceptors.request.eject(requeestInterceptor); // 原本會印出資料到 console 的被 eject 後就不見了
+axios.interceptors.response.eject(responseInterceptor);
 
 new Vue({
   el: '#app',
